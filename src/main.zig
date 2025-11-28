2a556c67be959e52206b6c3c43a869ecdf732147
Robert Stefanic <rstefanic72@gmail.com>
2025-11-28 12:58:13 -0500

    Generic JSON parsing
    Just getting a feel for JSON parsing in Zig.

--- src/main.zig
+++ src/main.zig
@@ -1,4 +1,6 @@
 const std = @import("std");
+const Allocator = std.mem.Allocator;
+
 const dependencygraph = @import("dependencygraph");
 
 pub fn main() !void {
@@ -6,7 +8,13 @@ pub fn main() !void {
     var stdout_writer = std.fs.File.stdout().writer(&stdout_buffer);
     const stdout = &stdout_writer.interface;
 
-    try stdout.print("Hello!\n", .{});
+    var buffer: [2048]u8 = undefined;
+    var fba = std.heap.FixedBufferAllocator.init(&buffer);
+    const allocator = fba.allocator();
 
+    const parsed_package = try dependencygraph.readPackageJson(allocator, "package-lock.json");
+    defer parsed_package.deinit();
+
+    try stdout.print("{s}!\n", .{ parsed_package.value.object.get("name").?.string });
     try stdout.flush(); // Don't forget to flush!
 }
--- src/root.zig
+++ src/root.zig
@@ -1,1 +1,16 @@
 const std = @import("std");
+
+const PackageJson = struct {
+    name: []const u8,
+    version: []const u8,
+    lockfileVersion: u8,
+    requires: bool,
+};
+
+pub fn readPackageJson(allocator: std.mem.Allocator, path: []const u8) !std.json.Parsed(std.json.Value) {
+    const buffer = try allocator.alloc(u8, 512);
+    defer allocator.free(buffer);
+    const data = try std.fs.cwd().readFile(path, buffer);
+
+    return std.json.parseFromSlice(std.json.Value, allocator, data, .{});
+}
