c5bbb8419b57c4922ddbf6617d7ecb052ef3e08c
Robert Stefanic <rstefanic72@gmail.com>
2026-05-04 09:34:10 -0400

    Create dvui window and wire up init/deinit/frame functions

--- src/main.zig
+++ src/main.zig
@@ -1,18 +1,40 @@
 const std = @import("std");
 const assert = std.debug.assert;
+const dvui = @import("dvui");
 const Allocator = std.mem.Allocator;
 
 const dependencygraph = @import("dependencygraph");
 
-pub fn main(init: std.process.Init) !void {
-    const io = init.io;
+pub const dvui_app: dvui.App = .{
+    .config = .{ .options = .{
+        .size = .{ .w = 800.0, .h = 600.0 },
+        .min_size = .{ .w = 800.0, .h = 600.0 },
+        .max_size = .{ .w = 800.0, .h = 600.0 },
+        .title = "Dependency Graph",
+    } },
+    .frameFn = appFrame,
+    .initFn = appInit,
+    .deinitFn = appDeinit,
+};
+
+pub const main = dvui.App.main;
+pub const panic = dvui.App.panic;
+pub const std_options: std.Options = .{ .logFn = dvui.App.logFn };
+
+var arena_allocator: std.heap.ArenaAllocator = undefined;
+var orig_content_scale: f32 = 1.0;
+
+pub fn appInit(win: *dvui.Window) !void {
+    orig_content_scale = win.content_scale;
+    arena_allocator = .init(std.heap.page_allocator);
+
+    // Parse the package lock file at the start .
+    const io = dvui.App.main_init.?.io;
     var stdout_buffer: [1024]u8 = undefined;
     var stdout_file_writer: std.Io.File.Writer = .init(.stdout(), io, &stdout_buffer);
     const stdout = &stdout_file_writer.interface;
 
-    var gpa = std.heap.DebugAllocator(.{}){};
-    const allocator = gpa.allocator();
-
+    const allocator = arena_allocator.allocator();
     var package = try dependencygraph.Package.init(io, allocator, "package-lock.json");
     defer package.deinit();
 
@@ -29,4 +51,15 @@ pub fn main(init: std.process.Init) !void {
 
     try stdout.print("Dependencies count for {s}: {d}\n", .{ package.name, package.packages.count() });
     try stdout.flush(); // Don't forget to flush!
+}
+
+pub fn appDeinit() void {
+    arena_allocator.deinit();
+}
+
+pub fn appFrame() !dvui.App.Result {
+    var tl = dvui.textLayout(@src(), .{}, .{ .expand = .horizontal, .font = .theme(.title) });
+    defer tl.deinit();
+    tl.addText("Hello world!", .{});
+    return .ok;
 }
