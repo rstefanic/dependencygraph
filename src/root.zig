4b0ad16c47ff47004348225f50a749aca8e12966
Robert Stefanic <rstefanic72@gmail.com>
2026-05-24 18:58:41 -0400

    Refactor Lockfile parsing to rename variable and strink variable scopes

--- src/root.zig
+++ src/root.zig
@@ -61,74 +61,80 @@ pub const LockFile = struct {
         const file = try std.Io.Dir.cwd().openFile(io, path, .{ .mode = .read_only });
         defer file.close(io);
 
+        // Read the file into a buffer.
         const stat = try file.stat(io);
         const size = stat.size;
         const buffer = try allocator.alloc(u8, size);
-
         _ = try file.readPositionalAll(io, buffer, 0);
 
-        // Grab the base object
+        // Grab the base JSON object.
         const json = try std.json.parseFromSlice(std.json.Value, allocator, buffer, .{});
-        const package = json.value.object;
+        const lockfile = json.value.object;
 
-        // Pull out the top level properties
-        const name = package.get("name").?.string;
-        const version = package.get("version").?.string;
-        const lockfile_version = package.get("lockfileVersion").?.integer;
-        const requires = package.get("requires").?.bool;
-
-        if (lockfile_version != 3) {
+        // Currently only support reading version 3 lockfiles.
+        const lockfile_version = lockfile.get("lockfileVersion") orelse {
+            return error.MissingLockfileVersion;
+        };
+        assert(lockfile_version == .integer);
+        if (lockfile_version.integer != 3) {
             return error.LockfileVersionNotSupported;
         }
 
-        const packages_obj = package.get("packages") orelse {
-            return error.MissingPackagesField;
-        };
-
-        var packages_it = packages_obj.object.iterator();
+        // This is our hash map of packages by name.
         var packages = std.StringHashMap(Package).init(allocator);
         errdefer packages.deinit();
 
-        while (packages_it.next()) |pkg| {
-            const pkg_key = pkg.key_ptr.*;
-            // The empty package name is the root package. While this is a valid JSON key,
-            // it's weird and I prefer calling it the root package.
-            var pkg_name = if (std.mem.eql(u8, pkg_key, "")) "root" else pkg_key;
-
-            // Drop the leading "node_modules/" from the name if it exists.
-            if (std.mem.startsWith(u8, pkg_name, "node_modules/")) {
-                pkg_name = pkg_name[13..];
-            }
-
+        // Iterate throught the "packages" object on the lockfile building
+        // up our own model of it.
+        const packages_json = lockfile.get("packages") orelse {
+            return error.MissingPackagesField;
+        };
+        var packages_json_it = packages_json.object.iterator();
+        while (packages_json_it.next()) |pkg| {
             assert(pkg.value_ptr.* == .object);
-            const dep_obj = pkg.value_ptr.*.object;
 
-            var dep = Package{
-                .version = if (dep_obj.get("version")) |dep_version| dep_version.string else null,
-                .resolved = if (dep_obj.get("resolved")) |resolved| resolved.string else null,
-                .integrity = if (dep_obj.get("integrity")) |integrity| integrity.string else null,
-                .link = if (dep_obj.get("link")) |link| link.bool else null,
-                .dev = if (dep_obj.get("dev")) |dev| dev.bool else null,
-                .optional = if (dep_obj.get("optional")) |optional| optional.bool else null,
-                .dev_optional = if (dep_obj.get("dev_optional")) |dev_optional| dev_optional.bool else null,
-                .in_bundle = if (dep_obj.get("in_bundle")) |in_bundle| in_bundle.bool else null,
-                .has_install_script = if (dep_obj.get("has_install_script")) |has_install_script| has_install_script.bool else null,
-                .has_shrinkwrap = if (dep_obj.get("has_shrinkwrap")) |has_shrinkwrap| has_shrinkwrap.bool else null,
-                .license = if (dep_obj.get("license")) |license| license.string else null,
+            const package_json = pkg.value_ptr.*.object;
+            var package = Package{
+                .version = if (package_json.get("version")) |version| version.string else null,
+                .resolved = if (package_json.get("resolved")) |resolved| resolved.string else null,
+                .integrity = if (package_json.get("integrity")) |integrity| integrity.string else null,
+                .link = if (package_json.get("link")) |link| link.bool else null,
+                .dev = if (package_json.get("dev")) |dev| dev.bool else null,
+                .optional = if (package_json.get("optional")) |optional| optional.bool else null,
+                .dev_optional = if (package_json.get("dev_optional")) |dev_optional| dev_optional.bool else null,
+                .in_bundle = if (package_json.get("in_bundle")) |in_bundle| in_bundle.bool else null,
+                .has_install_script = if (package_json.get("has_install_script")) |has_install_script| has_install_script.bool else null,
+                .has_shrinkwrap = if (package_json.get("has_shrinkwrap")) |has_shrinkwrap| has_shrinkwrap.bool else null,
+                .license = if (package_json.get("license")) |license| license.string else null,
             };
 
-            try addHashmapIfFieldExists(allocator, &dep.bin, dep_obj.get("bin"));
-            try addHashmapIfFieldExists(allocator, &dep.engines, dep_obj.get("engines"));
+            // Parse out the objects on the package as hashmaps.
+            try addHashmapIfFieldExists(allocator, &package.bin, package_json.get("bin"));
+            try addHashmapIfFieldExists(allocator, &package.engines, package_json.get("engines"));
+            try addHashmapIfFieldExists(allocator, &package.dependencies, package_json.get("dependencies"));
+            try addHashmapIfFieldExists(allocator, &package.dev_dependencies, package_json.get("devDependencies"));
+            try addHashmapIfFieldExists(allocator, &package.peer_dependencies, package_json.get("peerDependencies"));
+            try addHashmapIfFieldExists(allocator, &package.optional_dependencies, package_json.get("optionalDependencies"));
 
-            try addHashmapIfFieldExists(allocator, &dep.dependencies, dep_obj.get("dependencies"));
-            try addHashmapIfFieldExists(allocator, &dep.dev_dependencies, dep_obj.get("devDependencies"));
-            try addHashmapIfFieldExists(allocator, &dep.peer_dependencies, dep_obj.get("peerDependencies"));
-            try addHashmapIfFieldExists(allocator, &dep.optional_dependencies, dep_obj.get("optionalDependencies"));
+            // Create the name that we can use to reference the package.
+            const package_name = pkg.key_ptr.*;
+            // The empty package name is the root package. While this is a valid JSON key,
+            // it's weird and I prefer calling it the root package.
+            var name = if (std.mem.eql(u8, package_name, "")) "root" else package_name;
+            // Drop the leading "node_modules/" from the name if it exists.
+            if (std.mem.startsWith(u8, name, "node_modules/")) {
+                name = name[13..];
+            }
 
-            try packages.put(pkg_name, dep);
+            try packages.put(name, package);
         }
 
-        return .{ .allocator = allocator, .name = name, .version = version, .lockfile_version = lockfile_version, .requires = requires, .packages = packages };
+        // Grab high level information about the lockfile.
+        const name = lockfile.get("name").?.string;
+        const version = lockfile.get("version").?.string;
+        const requires = lockfile.get("requires").?.bool;
+
+        return .{ .allocator = allocator, .name = name, .version = version, .lockfile_version = lockfile_version.integer, .requires = requires, .packages = packages };
     }
 
     /// If the JSON object passed in exists, then a StringHashMap will be
